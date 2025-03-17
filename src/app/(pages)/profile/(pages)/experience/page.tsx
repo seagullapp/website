f6e5148ea8cc39529a3b7308ef25e47da754367d
Taha Paracha <parachataha2@gmail.com>
2025-03-17 21:15:18 +0400

    Basic user owned organization list

--- src/app/(pages)/profile/(pages)/experience/page.tsx
+++ src/app/(pages)/profile/(pages)/experience/page.tsx
@@ -25,13 +25,6 @@ export default function UserExperiencePage() {
 
         {showAddExperience && <AddExperience setShow={setShowAddExperience} />}
         
-        {/* <header className='mb-2'>
-            <h2> {user.experience?.length > 0 ? "Your Experience" : "You have no experience yet"}  </h2>
-            {user.experience?.length <= 0 && <p className='grey'> With no experience, the experience tab will not show on your profile page </p>}
-        </header>
-        
-        {user.experience?.length <= 0 && <HorizontalAdd onClick={() => router.push('/profile/experience/create')} className='w-full py-[20px_!important]'> Create Experience List </HorizontalAdd> } */}
-
         <h3 className='font-semibold'> Your Experience </h3>
         <p className="grey mb-2"> Share your experience with the world </p>
         {user.experience?.length > 0 ? 
