7533011409a68f41d79439402b89b7c06200c060
Taha Paracha <parachataha2@gmail.com>
2025-03-18 22:40:56 +0400

    Style: Shadows to dropdown and Nav Modal

--- src/app/components/buttons/Dropdown/Dropdown.tsx
+++ src/app/components/buttons/Dropdown/Dropdown.tsx
@@ -30,7 +30,7 @@ export default function Dropdown( {parentButton, items} : Props ) {
 
             <button className={parentButton?.className} onClick={() => setShow(show ? false : true)}> {parentButton.text} </button>
 
-            {show && <div className='w-max flex flex-col bg-secondary-bg rounded-lg border-2 border-tertiary-bg absolute top-[115%] left-[50%] translate-x-[-50%] z-10'> 
+            {show && <div className='w-max shadow-xl flex flex-col bg-secondary-bg rounded-lg border-2 border-tertiary-bg absolute top-[115%] left-[50%] translate-x-[-50%] z-10'> 
                 
                 {items.map((item, index) => {
                     if (item?.link) { return <Link href={`${item.href}`} className={`text-white/80 z-10 p-1 ${index !== items.length - 1 ? "border-b-2 border-b-white/10" : ""}`} key={index}>
