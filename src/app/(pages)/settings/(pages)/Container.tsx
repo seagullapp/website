33716bb893a87cebdcba51ec16f768ee98eabd15
Taha Paracha <parachataha2@gmail.com>
2025-03-21 16:35:07 +0400

    Feat: Basic settings page

--- src/app/(pages)/settings/(pages)/Container.tsx
+++ src/app/(pages)/settings/(pages)/Container.tsx
@@ -1,17 +1,25 @@
 "use client"
 import logoutUser from "@/lib/auth/logoutUser";
+import capitalize from "@/utils/capitalize";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 // Redux
-import { clearUser } from "@/app/redux/slices/userSlice";
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { clearUser } from "@/app/redux/slices/userSlice";
+
+// Types
+import { RootState } from "@/app/redux/store";
+import Avatar from "@/app/components/User/Avatar/Avatar";
 
 export default function Container() {
 
     const router = useRouter()
     const dispatch = useDispatch()
     const [error, setError] = useState<{isError: Boolean, msg: string}>({isError: false, msg: "An error occurred"})
+
+    const user = useSelector((root: RootState) => root.user);
 
     async function handleLogout() {
 
@@ -34,14 +42,32 @@ export default function Container() {
 
     return ( <div className="">
 
-        <h3></h3>
+        <form className="">
+            
+            <div className="flex justify-between items-center gap-4">
+                <div className="flex flex-col flex-grow max-w-[400px]"> 
+                    <label htmlFor="first-name" className=''> First name </label>
+                    <input name='first-name' type="text" placeholder={capitalize(user.firstName)} />
+                    <label htmlFor="last-name" className='mt-3'> Last name </label>
+                    <input name='last-name' type="text" placeholder={capitalize(user.lastName)} />
+                </div>
+                <Avatar avatar_url={user.avatar_url} avatar={user.avatar} size={100} />
+            </div>
 
-        <button onClick={handleLogout}> Log out </button>
+            <div className='mt-4'>
 
-        {error.isError && <div> 
-            Error!
-            <p> {error.msg} </p>
-        </div>}
+                <h3 className='text-[20px] grey font-semibold mb-1'> Danger Zone </h3>
+                <button onClick={handleLogout}> Log out </button>
+
+                {error.isError && <div> 
+                    Error!
+                    <p> {error.msg} </p>
+                </div>}
+
+            </div>
+
+
+        </form>
 
     </div> )
 }
